db59ee85cbfc2ae33afe9b2e109eb2ff90795e74
rallker1337 <rass1994@gmail.com>
2021-03-24 16:51:31 +0100

    jumbotro

--- nmhi/js/animations.js
+++ nmhi/js/animations.js
@@ -1,35 +1,42 @@
 $(".widget").fadeToggle(0);
 $(".widget").fadeToggle(2000);
 
-function updateWidgets(){
-    $(".lower-widget").children().fadeToggle(0);
-    let boxHeight = $(".widget").height();
-    $(".widget").mouseenter(function(){
-            $(this)
-            .stop(true, true)
-            .animate({
-                height: "122",
-                margin: "0 16 0 16"
-            }, 120)
-            .find(".lower-widget")
-            .children()
-            .stop(true)
-            .fadeToggle(200, console.log("a"));
-        })
-        .mouseleave(function(){
-            $(this)
-            .stop(true, false)
-            .animate({
-                height: boxHeight,
-                margin: "16 16 16 16"
-            }, 180)
-            .find(".lower-widget")
-            .children()
-            .stop(true)
-            .fadeToggle(160, console.log("b"));
-        });
-        
-        // $(".widget").click(function(){
-        //     $(this).find(".temp").fadeToggle(200);
-        // });
-    };
+function updateWidgets() {
+  $(".lower-widget").children().fadeToggle(0);
+  let boxHeight = $(".widget").height();
+  $(".widget")
+    .mouseenter(function () {
+      $(this)
+        .stop(true, true)
+        .animate(
+          {
+            height: "122",
+            margin: "0 16 0 16",
+          },
+          120
+        )
+        .find(".lower-widget")
+        .children()
+        .stop(true)
+        .fadeToggle(200, console.log("a"));
+    })
+    .mouseleave(function () {
+      $(this)
+        .stop(true, false)
+        .animate(
+          {
+            height: boxHeight,
+            margin: "16 16 16 16",
+          },
+          180
+        )
+        .find(".lower-widget")
+        .children()
+        .stop(true)
+        .fadeToggle(160, console.log("b"));
+    });
+
+  // $(".widget").click(function(){
+  //     $(this).find(".temp").fadeToggle(200);
+  // });
+}
